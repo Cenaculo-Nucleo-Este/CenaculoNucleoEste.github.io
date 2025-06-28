d8a8b518f5731e4c3f1e8c842f95956643f35025
Ric <henriqueft04@ua.pt>
2025-06-28 20:54:05 +0100

    we try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import staticAdapter from '@astrojs/adapter-static';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cenaculonucleoeste.github.io",
-  base: "/",
+  adapter: staticAdapter(),
+  base: "/CenaculoNucleoEste.github.io/",
   integrations: [tailwind()],
 });
