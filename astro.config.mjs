c42e3e05940ddac7b9b4bf806f40063a8e40c0ef
Ric <henriqueft04@ua.pt>
2025-06-29 01:50:47 +0100

    try deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import staticAdapter from '@astrojs/adapter-static';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cenaculonucleoeste.github.io",
-  adapter: staticAdapter(),
-  base: "/CenaculoNucleoEste.github.io/",
+  base: "/",
   integrations: [tailwind()],
 });
